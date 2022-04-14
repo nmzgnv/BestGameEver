a89cb945717c361f9aeea1fdee03e3c88bb2dd7e
nmzgnv <nick-mozg1@mail.ru>
2022-04-14 11:46:47 +0500

    Split logic

--- Assets/Scripts/Player/PhysicsMovement.cs
+++ Assets/Scripts/Player/PhysicsMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -7,35 +6,25 @@ public class PhysicsMovement : MonoBehaviour
     [SerializeField]
     private float speed;
 
-    [SerializeField]
-    private string runAnimationParameterName = "Optional";
-
     private Rigidbody2D _rigidbody;
-    private Animator _animator;
+    private Vector2 _previousPosition;
+
+    public Vector2 Velocity { get; private set; }
+
+    private void FixedUpdate()
+    {
+        Velocity = (_rigidbody.position - _previousPosition) / Time.fixedDeltaTime;
+        _previousPosition = _rigidbody.position;
+    }
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
     }
+
 
     public void Move(Vector2 direction)
     {
         _rigidbody.MovePosition(_rigidbody.position + direction * speed);
-        var horizontalDirection = direction.x;
-
-        if (horizontalDirection != 0 && Math.Sign(transform.localScale.x) != Math.Sign(horizontalDirection))
-            Flip();
-
-        if (_animator != null)
-            _animator.SetFloat(runAnimationParameterName, direction.magnitude);
-    }
-
-
-    private void Flip()
-    {
-        var scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
     }
 }
