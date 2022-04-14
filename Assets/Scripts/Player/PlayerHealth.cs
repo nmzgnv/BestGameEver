7cc9d1c8cdf00fc773c566f9147f3517ce4438a5
Rychkov Konstantin <k9536008655@gmail.com>
2022-04-15 04:39:36 +0500

    Add PlayerHealth.cs

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth;
+    private int curHealth;
+
+    [SerializeField]
+    private float invulnerableTime;
+    private float lastDamageTime;
+    private bool IsInvulnerable =>
+        Time.realtimeSinceStartup - lastDamageTime < invulnerableTime;
+
+    private void Start()
+    {
+        curHealth = maxHealth;
+        lastDamageTime = Time.realtimeSinceStartup;
+    }
+
+    public void ReceiveDamage()
+    {
+        if (IsInvulnerable)
+            return;
+
+        curHealth--;
+        lastDamageTime = Time.realtimeSinceStartup;
+
+        if (curHealth == 0)
+            Die();
+    }
+
+    public void ReceiveHeal()
+    {
+        if (curHealth < maxHealth)
+            curHealth++;
+    }
+    
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
