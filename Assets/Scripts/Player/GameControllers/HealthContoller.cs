5a2c84ce55fe20c8c06296b8e573ea536d342e5c
Khramov_Egor <khramovegor@gmail.com>
2022-04-22 14:08:12 +0500

    Updated teleportation and added controllers

---
+++ Assets/Scripts/Player/GameControllers/HealthContoller.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthContoller : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth;
+    private int _curHealth;
+    
+    
+}
