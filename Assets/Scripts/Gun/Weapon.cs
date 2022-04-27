03deb7c12a11b143581ac00c2c7a5be7c5cb512a
nmzgnv <nick-mozg1@mail.ru>
2022-04-28 00:02:56 +0500

    Add menu

--- Assets/Scripts/Gun/Weapon.cs
+++ Assets/Scripts/Gun/Weapon.cs
@@ -11,6 +11,12 @@ public class Weapon : MonoBehaviour
     [SerializeField]
     private Transform bulletSpawnPoint;
 
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip shotSound;
+
     private float _elapsed;
     private float _rateOfFire;
 
@@ -35,6 +41,9 @@ public class Weapon : MonoBehaviour
         {
             _elapsed %= _rateOfFire;
             Instantiate(bullet, bulletSpawnPoint.position, transform.rotation);
+            
+            audioSource.pitch = Random.Range(0.8f, 1.2f);
+            audioSource.PlayOneShot(shotSound, 0.6f);
         }
     }
 }
