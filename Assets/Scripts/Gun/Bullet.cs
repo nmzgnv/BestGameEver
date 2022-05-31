b209e2d5e0073bf889eecb1b8b26a13b758d87c4
Ignat <maratsalo2015@gmail.com>
2022-05-31 07:44:18 +0500

    fix some ui elements and bullets explosion

--- Assets/Scripts/Gun/Bullet.cs
+++ Assets/Scripts/Gun/Bullet.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 [RequireComponent(typeof(PhysicsMovement))]
 public class Bullet : BulletBase
 {
+    public Color DamageColor;
     public ParticleSystem boom;
 
     [SerializeField]
     private float destroyAfterSeconds = 2;
 
     private PhysicsMovement _physicsMovement;
-
+    private bool isDamageable = false;
     private void Start()
     {
         _physicsMovement = GetComponent<PhysicsMovement>();
@@ -25,11 +26,18 @@ public class Bullet : BulletBase
     public void OnCollisionEnter2D(Collision2D other)
     {
         Destroy(gameObject);
+        if (other.gameObject.layer == 10) // If player collision
+            isDamageable = true;
     }
 
     public void OnDestroy()
     {
         if (!gameObject.scene.isLoaded) return;
-        Destroy(Instantiate(boom.gameObject, transform.position, transform.rotation), destroyAfterSeconds);
+
+        var boomObj = Instantiate(boom.gameObject, transform.position, transform.rotation);
+        if (isDamageable)
+            boomObj.GetComponent<ParticleSystem>().startColor = DamageColor;
+
+        Destroy(boomObj, destroyAfterSeconds);
     }
 }
