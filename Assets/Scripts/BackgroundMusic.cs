97b86394fc30a24f668ec2de5d4892420ef348cb
nmzgnv <nick-mozg1@mail.ru>
2022-06-05 20:34:16 +0500

    Fix music

--- Assets/Scripts/BackgroundMusic.cs
+++ Assets/Scripts/BackgroundMusic.cs
@@ -1,14 +1,25 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BackgroundMusic : MonoBehaviour
 {
+    private static BackgroundMusic s_instance = null;
+
     [SerializeField]
     private string bossLevelName = "level_8";
 
-    private void Awake()
+    private void Start()
     {
-        DontDestroyOnLoad(this);
+        if (s_instance == null)
+            s_instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
 
     private void OnEnable()
@@ -18,7 +29,11 @@ public class BackgroundMusic : MonoBehaviour
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if (scene.name == bossLevelName)
-            Destroy(gameObject);
+        gameObject.SetActive(scene.name != bossLevelName);
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
